49cb924ea9ab395cf77124267565c42d0adcadd4
tetema <tetema@student.wethinkcode.co.za>
2022-04-10 16:48:32 +0200

    added everything

---
+++ _site/assets/js/main.js
@@ -0,0 +1,16 @@
+let userDarkMode = localStorage.getItem('darkMode');
+var slider = document.getElementById('myCheckbox');
+
+const toggleDarkMode = () => localStorage.setItem('darkMode', 'onDark')
+
+if (userDarkMode === 'darkMode') toggleDarkMode();
+
+const toggleLightMode = () => localStorage.setItem('darkMode', null);
+
+slider.addEventListener('click', () => {
+    userDarkMode = localStorage.getItem('darkMode');
+    if (userDarkMode !== 'onDark' && slider.checked ) toggleDarkMode()
+    else toggleLightMode();
+});
+
+
---
+++ about.markdown
@@ -0,0 +1,7 @@
+---
+layout: page
+title: About Tebogo
+permalink: /about/
+---
+
+I am a `Software Engineer` in progress.
